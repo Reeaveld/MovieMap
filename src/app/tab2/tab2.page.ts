04dc504188cc341dea2d51cea928b47b276f2233
rifqi <rifqialrasid71@gmail.com>
2024-11-17 22:01:19 +0700

    sinkronin film

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,10 @@
 // tab2.page.ts
 import { Component, OnInit } from '@angular/core';
 import { OmdbService } from '../services/omdb.service';
+import { Router } from '@angular/router';
 
 interface Review {
+  imdbID: string;
   movieTitle: string;
   year: string;
   posterUrl: string;
@@ -19,43 +21,71 @@ interface Review {
 })
 export class Tab2Page implements OnInit {
   selectedSegment = 'REVIEWS';
-  reviews: Review[] = [
+  reviews: Review[] = [];
+  error: string = '';
+  
+  // cara cari ID = cari filmnya > https://www.imdb.com/title/[ID Filmnya]/
+  mockReviews = [
     {
-      movieTitle: 'Anora',
-      year: '2024',
-      posterUrl: 'assets/placeholder-poster.jpg',
+      imdbID: 'tt21064584', // ID untuk "The Iron Claw"
+      reviewText: 'Mikey Madison is superb in this very unique effort. The comparisons don\'t hold up beyond some basic plot set-up, and after the stage is set, the film is utterly unpredictable, quite funny, and hides surprising depth.',
       rating: 4,
-      reviewText: 'Mikey Madison is superb in this very unique effort from Sean Baker. PRETTY WOMAN comparisons don\'t hold up beyond some basic plot set-up, and after the stage is set, the film is utterly unpredictable, quite funny, and hides surprising depth. One of the best I\'v...',
       reviewer: 'Mike',
       reviewerAvatar: 'assets/avatar1.jpg'
     },
     {
-      movieTitle: 'Look Back',
-      year: '2024',
-      posterUrl: 'assets/placeholder-poster.jpg',
+      imdbID: 'tt14230458', // ID untuk "Poor Things"
+      reviewText: 'We were gonna make so many things together. We were going to put out art that would matter to people and affect their lives. We were going to do it together.',
       rating: 4.5,
-      reviewText: 'We were gonna make so many things together. We were going to put out art that would matter to people and affect their lives. We were going to do it together. And now here I am, trying to commit to and focus on making something, anything. But it\'s just so ...',
       reviewer: 'James',
       reviewerAvatar: 'assets/avatar2.jpg'
     },
     {
-      movieTitle: 'Heretic',
-      year: '2024',
-      posterUrl: 'assets/placeholder-poster.jpg',
-      rating: 2.5,
+      imdbID: 'tt28015403', // ID untuk "heretic"
       reviewText: 'Jigsaw if he was an insufferable reddit atheist',
+      rating: 2.5,
       reviewer: 'Stacey',
       reviewerAvatar: 'assets/avatar3.jpg'
     }
   ];
 
-  constructor(private omdbService: OmdbService) {}
+  constructor(
+    private omdbService: OmdbService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    // You can fetch real reviews data here
+    this.loadReviews();
+  }
+
+  loadReviews() {
+    // Mengambil detail film untuk setiap review
+    this.mockReviews.forEach(review => {
+      this.omdbService.getMovieDetails(review.imdbID).subscribe(
+        (movieData) => {
+          if (movieData.Response === 'True') {
+            this.reviews.push({
+              ...review,
+              movieTitle: movieData.Title,
+              year: movieData.Year,
+              posterUrl: movieData.Poster !== 'N/A' ? movieData.Poster : 'assets/placeholder-poster.jpg',
+              imdbID: review.imdbID
+            });
+          }
+        },
+        (error) => {
+          this.error = 'Failed to fetch movie details';
+          console.error(error);
+        }
+      );
+    });
   }
 
   segmentChanged(event: any) {
     this.selectedSegment = event.detail.value;
+  }
+
+  goToMovieDetail(movieId: string) {
+    this.router.navigate(['/movie-detail', movieId]);
   }
 }
